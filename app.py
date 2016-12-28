7d24cb976b3260dcba7ab97a3423bc706479a1a5
gr <german@arriendas.cl>
2016-12-28 17:59:20 -0300

    la base de datos alimenta la vista del primer template

--- app.py
+++ app.py
@@ -4,6 +4,12 @@ import random
 import string
 import cherrypy
 import os
+from pymongo import MongoClient
+from datetime import datetime
+from pprint import pprint
+import sys  
+reload(sys)  
+sys.setdefaultencoding('utf8')
 
 class FrontEnd(object):
     @cherrypy.expose
@@ -12,11 +18,51 @@ class FrontEnd(object):
         return html
 
     @cherrypy.expose
-    def templates(self):
-	root = os.path.realpath(__file__)
-        templates = open("templates/a/template.html").read()
-        return templates 
+    def create(self):
+        client = MongoClient()
+        db = client.cherrypies        
+        result = db.users.insert_one(
+    {
+            "id": "5",
+            "name": "Ricardo Rimoldi",
+            "email": "surimo@fibertel.com.ar",
+            "address": "De la Angostura 63",
+            "telephone": "+54 911 44004075",
+            "company_name": "Inmobi"
+    })  
 
+# a la database le puse de nombre "cherrypies"
+    @cherrypy.expose
+    def db(self, id = 1):
+        brand = "Zia"
+        client = MongoClient()
+        db = client.cherrypies        
+        c = db.users.find({"id":id})
+        for document in c: 
+            email = document["email"]
+            name = document["name"]
+            company_name = document["company_name"]
+            telephone = document["telephone"]
+            address = document["address"]
+        template = open("templates/a/template.html").read()
+        return template.format(email=email,name=name,company_name=company_name,telephone=telephone,address=address,brand=brand)
+    
+
+    @cherrypy.expose
+    def user(self):
+        client = MongoClient()
+        db = client.cherrypies        
+        result = db.users.insert_one(
+    {
+            "id": id,
+            "name": name,
+            "email": email,
+            "address": address,
+            "company_name": company_name,
+            "telephone": telephone
+    })  
+        user = open("frontend/user.html").read()
+        return user 
 
 if __name__ == '__main__':
     conf = {
