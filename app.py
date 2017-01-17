3210f3fa98fbf3c8d73ccbd12a1ea5a673b1b310
Gerr <german@arriendas.cl>
2017-01-17 16:15:51 -0300

    diseno final y conexion a la base de datos parcial

--- app.py
+++ app.py
@@ -1,7 +1,6 @@
+# -*- coding: utf-8 -*- 
 # Estamos usando un sevidor HTTP que viene incluido en el framwork de Cherrypy
 # Usamos como base de datos MONGODB. La base de datos se llama "cherrypies".
-
-# -*- coding: utf-8 -*- 
 import random
 import json
 import string
@@ -17,20 +16,43 @@ reload(sys)
 sys.setdefaultencoding('utf8')
 
 class FrontEnd(object):
-    name = "Zucar"
+    name = "TusWebs.cl"
     telephone = "22 938 1010"
     address = "La Concepcion 81"
     commune = "Providencia"
     city = "Santiago"
     country = "Chile"
+    js = """var parallax_map;
+        $().ready(function(){
+            responsive = $(window).width();
+        
+            examples.initContactUsMap();
+            
+            if (responsive >= 768){
+                parallax_map = $('.parallax').find('.big-map');
+                
+                $(window).on('scroll',function(){           
+                    parallax();
+                    gsdk.checkScrollForTransparentNavbar();
+                });
+            }
+            
+        });        
+
+       var parallax = function() {
+            var current_scroll = $(this).scrollTop();
+            
+            oVal = ($(window).scrollTop() / 3); 
+            parallax_map.css('top',oVal);
+        };"""
 
 # Vistas basicas 
     @cherrypy.expose
     def index(self):
-        header = open('frontend/header.html').read()
-        nav = open('frontend/nav.html').read() 
+        header = open('frontend/views/header.html').read()
+        nav = open('frontend/views/nav.html').read() 
         body = open('frontend/index.html').read()
-        footer = open('frontend/footer.html').read()
+        footer = open('frontend/views/footer.html').read()
         return header.format(),nav.format(our=FrontEnd),body.format(our=FrontEnd),footer.format(our=FrontEnd)
 
 # Paso 0: carga manual de datos de prueba en la base de datos
@@ -47,7 +69,46 @@ class FrontEnd(object):
                 "name": "Tu Empresa",
                 "email": "soporte@tuempresa.cl",
                 "telephone": "22 200 2000",
-                "address": "Tu Direccion Comercial 100"
+                "address": "Tu Direccion Comercial 100",
+                "webpage": {
+                    "title_1": "Estudio Contable Profesional en Santiago de Chile",
+                    "tag_line_1":"",
+                    "menu_1":"Inicio",
+                    "menu_2":"Propuesta y servicios",
+                    "menu_3":"Clientes",
+                    "menu_4":"Contacto",
+                    "summary_title_1":"Nuestra Propuesta",
+                    "summary_content_1":"El objetivo de su empresa es maximizar los retornos para sus accionistas. Trabajamos para ayudarle a conseguirlo.",
+                    "proposition_title_1": "Servicios contables",
+                    "proposition_title_2": "Asesoría y planificación tributaria",
+                    "proposition_title_3": "Auditoría",
+                    "proposition_title_4": "Servicios financieros",
+                    "proposition_content_1": "Declaración mensual de impuestos",
+                    "proposition_content_2": "Planificamos sus tributaciones para disminuir el pago de rentas",
+                    "proposition_content_3": "Auditoria de sus Estados Financieros",
+                    "proposition_content_4": "Asesoramiento en la presentación de proyectos frente a inversionistas",
+                    "service_title_1":"Nuestros servicios",
+                    "service_subtitle_1":"Distintos servicios standard",
+                    "pricing_title_1":"Estándard",
+                    "pricing_title_2":"Contable, impositivo y legal",
+                    "pricing_title_3":"Servicios Específicos",
+                    "pricing_content_1":"Estándard1",
+                    "pricing_content_2":"Estándard2",
+                    "pricing_content_3":"Estándard3",
+                    "pricing_content_4":"Estándard4",
+                    "pricing_content_5":"Estándard5",
+                    "pricing_content_6":"Estándard6",
+                    "customer_title_1":"Lo que nuestros clientes dicen",
+                    "customer_subtitle_1":"Lo que nuestros clientes dicen",
+                    "customer_content_1":"Lo que nuestros clientes dicen",
+                    "customer_content_2":"Lo que nuestros clientes dicen",
+                    "customer_content_3":"Lo que nuestros clientes dicen",  
+                    "about_title_1:": "Sobre Nosotros",
+                    "about_content_1": "Estudio contable con años de experiencia asesorando tributariamente a nuestros clientes",
+                    "contact_title_1":"Contáctanos",
+                    "contact_content_1":"Escribenos para agendar una reunión y comprender más nuestros servicio",
+                    "action":"Ver más"
+                    }
                 }
     })  
         return "Objeto creado"
@@ -81,8 +142,9 @@ class FrontEnd(object):
                 }
         }) #primero se graban los datos y luego se trae y reorganiza con formato de objeto para pasar a la vista 
         cursor = db.users.find({"company.email":company_email})
+        User = namedtuple('User', 'email telephone, company') #uso namedtuple para llamar los resultados como objetos con notacion con puntos, como: "company.email"
         Company = namedtuple('company', 'email name telephone address')
-        User = namedtuple('User', 'email, company') #uso namedtuple para llamar los resultados como objetos con notacion con puntos, como: "company.email"
+#        Webpage = namedtuple('webpage', )
         for row in cursor:
             user = User(row['email'], Company(row['company']['email'],row['company']['name'],row['company']['telephone'],row['company']['address'])) #esta es la sintaxis necesaria para usar nametupe
         design_route = "templates/b/index.html"
@@ -91,17 +153,54 @@ class FrontEnd(object):
     
 # Opcion Marketing B: Generar vista del template sin que el usuario este registrado
     @cherrypy.expose
+    def example_create(self):
+        client = MongoClient()
+        db = client.cherrypies        
+        result = db.users.insert_one(
+        {
+            "email": "email@email.com",
+            "telephone": "22 200 2000",
+            "password": "",
+            "company": {
+                "name": "Tu Empresa",
+                "email": "prueba@tuempresa.cl",
+                "telephone": "22 200 2000",
+                "address": "Tu Direccion Comercial 100",
+                "webpage": {
+                    "title": "Estudio Contable Profesional en Santiago de Chile"
+                           }
+                        }    
+        }) 
+        return "Objeto creado"
+
+# Opcion Marketing B: Generar vista del template sin que el usuario este registrado
+    @cherrypy.expose
+    def example_view(self):
+        client = MongoClient()
+        db = client.cherrypies
+        cursor = db.users.find({"company.email":"prueba@tuempresa.cl"})
+        User = namedtuple('User', 'email telephone, company') #uso namedtuple para llamar los resultados como objetos con notacion con puntos, como: "company.email"
+        Company = namedtuple('company', 'email name telephone address, webpage')
+        Webpage = namedtuple('webpage', 'title') 
+        for row in cursor:
+            user = User(row['email'],row['telephone'], Company(row['company']['email'],row['company']['name'],row['company']['telephone'],row['company']['address'],Webpage(row['company']['webpage']['title']))) #esta es la sintaxis necesaria para usar nametupe
+        design_route = "templates/a/1.html"
+        template = open(design_route).read()
+        return template.format(user=user, our=FrontEnd)
+
+    @cherrypy.expose
     def unregistered(self):
         client = MongoClient()
         db = client.cherrypies
         cursor = db.users.find({"company.email":"soporte@tuempresa.cl"})
-        Company = namedtuple('company', 'email name telephone address')
         User = namedtuple('User', 'email, company') #uso namedtuple para llamar los resultados como objetos con notacion con puntos, como: "company.email"
+        Company = namedtuple('company', 'email name telephone address')
+        Webpage = namedtuple('webpage', 'title') 
         for row in cursor:
-            user = User(row['email'], Company(row['company']['name'],row['company']['email'],row['company']['telephone'],row['company']['address'])) #esta es la sintaxis necesaria para usar nametupe
-        design_route = "templates/b/index.html"
+            user = User(row['email'], Company(row['company']['email'],row['company']['name'],row['company']['telephone'],row['company']['address'])) #esta es la sintaxis necesaria para usar nametupe
+        design_route = "templates/a/index.html"
         template = open(design_route).read()
-        return template.format(user=user)
+        return template.format(user=user, our=FrontEnd)
 
 # Opcion Marketing C: Generar vista del template sin que el usuario este registrado
     @cherrypy.expose
@@ -124,7 +223,7 @@ class FrontEnd(object):
         User = namedtuple('User', 'email, company') #uso namedtuple para llamar los resultados como objetos con notacion con puntos, como: "company.email"
         for row in cursor:
             user = User(row['email'], Company(row['company']['email'],row['company']['name'],row['company']['telephone'],row['company']['address'])) #esta es la sintaxis necesaria para usar nametupe
-        design_route = "templates/b/index.html"
+        design_route = "templates/a/index.html"
         template = open(design_route).read()
         return template.format(user=user)
     
@@ -149,10 +248,6 @@ if __name__ == '__main__':
             'tools.staticdir.dir': './frontend/public'
         },
         '/root': {
-            'tools.staticdir.on': True,
-            'tools.staticdir.dir': './templates/b'
-        },
-        '/route': {
             'tools.staticdir.on': True,
             'tools.staticdir.dir': './templates/a'
         }
